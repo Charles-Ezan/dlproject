2aff2c06ef98a0042f8da335b2c43dc84b9c6bb4
Charles Ezan <charles.ezan1@uqac.ca>
2021-06-15 15:08:30 -0400

    Database & dependence update

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+df = pd.read_csv('bitcoin_2011-1-1_2019-1-1.csv', header=None)
+df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap']
 
+y = df['Close'].values
+X = df[['Volume']].values
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+regr = LinearRegression()
 
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.scatter(X, y, label='training points', color='lightgray')
+
+plt.plot(X_fit, y_lin_fit,
+         label='linear (d=1), $R^2=%.2f$' % linear_r2,
+         color='blue',
+         lw=2,
+         linestyle=':')
+
+plt.ylabel('% Clode')
+plt.xlabel('Date')
+plt.legend(loc='upper right')
+
+plt.tight_layout()
+plt.show()
